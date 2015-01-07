7ad3a461b88cc8b70354ec72fbe3e113bf39c255
mosszhaodphil <mosszhaodphil@gmail.com>
2015-01-07 18:01:48 +0000

    need to implement save images

--- plot_perfusion_map.m
+++ plot_perfusion_map.m
@@ -1,10 +1,12 @@
-
+% This funciton generates the montage of all the slices of perfusion maps along z direction
+% Input parameter: 3D perfusion matrix
 
 function [] = plot_perfusion_map(perfusion_matrix)
 
-	%perfusion_matrix_abs = abs(perfusion_matrix);
-	[x, y, z] = size(perfusion_matrix);
+	[x, y, z] = size(perfusion_matrix); % get the dimension of the perfusion matrix
 
+	% Some perfusion values can be negative
+	% We assign zero to these voxels
 	for i = 1 : x
 		for j = 1 : y
 			for k = 1 : z
@@ -15,12 +17,12 @@ function [] = plot_perfusion_map(perfusion_matrix)
 		end
 	end
 
+	% In Matlab, the montage volume must be in x by y by 1 by k dimension
+	% K is the number of slices, in this case z
 	montage_volume = zeros(x, y, 1, z);
-
 	for j = 1 : z
 		montage_volume(:, :, 1, j) = perfusion_matrix(:, :, j);
 	end
 
-	montage(montage_volume, colormap('hot'));
-	colorbar;
 end
+
