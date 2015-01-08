3d488a7bcdea6219218be3d1239fb95eec87371c
mosszhaodphil <mosszhao@Mosss-MacBook-Pro-2.local>
2015-01-08 11:52:51 +0000

    rename rotate_perfusion_matrix to rotate_matrix
    A more generic function signature

--- rotate_matrix.m
+++ rotate_matrix.m
@@ -1,16 +1,16 @@
 % This function rotate a 3D matrix by rotate_angle degrees counterclock wise.
 % The function assumes z direction is fixed.
-% perfusion_matrix(:, :, z) is to be rotated.
+% input_matrix(:, :, z) is to be rotated.
 % rotate_angle is the angle (in degrees) to be rotated.
 
-function perfusion_matrix_rotate = rotate_perfusion_matrix(perfusion_matrix, rotate_angle)
+function output_matrix = rotate_matrix(input_matrix, rotate_angle)
 
 	% Construct an empty matrix to save the rotation result
-	[x, y, z] = size(perfusion_matrix);
-	perfusion_matrix_rotate = zeros(x, y, z);
+	[x, y, z] = size(input_matrix);
+	output_matrix = zeros(x, y, z);
 
 	% rotate perfusion matrix to rotate_angle degrees
 	for i = 1 : z
-		perfusion_matrix_rotate(:, :, i) = imrotate(perfusion_matrix(:, :, i), rotate_angle);
+		output_matrix(:, :, i) = imrotate(input_matrix(:, :, i), rotate_angle);
 	end
 end
