37eb5e4c6b979cde5a227dd422d125cca0506f18
mosszhaodphil <mosszhao@Mosss-MacBook-Pro.local>
2015-02-08 22:10:09 +0000

    refactor plot_perfusion_map function

--- plot_perfusion_map.m
+++ plot_perfusion_map.m
@@ -1,17 +1,19 @@
-% This funciton generates the montage of all the slices of perfusion maps along z direction
+% This funciton generates the montage of all the slices of perfusion maps along z direction.
+% All negative voxels are converted to zero
+% The function returns the figur handle of the montage (must be saved manually).
 % Input parameter: 3D perfusion matrix
 
-function [] = plot_perfusion_map(perfusion_matrix)
+function figure_handle = plot_montage(input_matrix, colormap_type)
 
-	[x, y, z] = size(perfusion_matrix); % get the dimension of the perfusion matrix
+	[x, y, z] = size(input_matrix); % get the dimension of the perfusion matrix
 
 	% Some perfusion values can be negative
 	% We assign zero to these voxels
 	for i = 1 : x
 		for j = 1 : y
 			for k = 1 : z
-				if(perfusion_matrix(i, j, k) <= 0)
-					perfusion_matrix(i, j, k) = 0;
+				if(input_matrix(i, j, k) <= 0)
+					input_matrix(i, j, k) = 0;
 				end
 			end
 		end
@@ -21,8 +23,13 @@ function [] = plot_perfusion_map(perfusion_matrix)
 	% K is the number of slices, in this case z
 	montage_volume = zeros(x, y, 1, z);
 	for j = 1 : z
-		montage_volume(:, :, 1, j) = perfusion_matrix(:, :, j);
+		montage_volume(:, :, 1, j) = input_matrix(:, :, j);
 	end
+
+	% Display the montage
+	figure;
+	figure_handle = montage(montage_volume, colormap(colormap_type));
+	colorbar;
 
 end
 
