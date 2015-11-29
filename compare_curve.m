dbd097841184389427f6030494471844545956d2
mosszhaodphil <mosszhao@Mosss-MacBook-Pro.local>
2015-11-29 21:09:24 +0000

    new function to compare curve

---
+++ compare_curve.m
@@ -0,0 +1,31 @@
+% This function compares two time series
+% It draws one on top of another
+
+function compare_curve(file_1, file_2)
+
+	file_extension = '.nii.gz';
+
+	file_handle_1 = load_nii(strcat(file_1, file_extension));
+	file_handle_2 = load_nii(strcat(file_2, file_extension));
+
+	matrix_1 = file_handle_1.img;
+	matrix_2 = file_handle_2.img;
+
+	vector_1 = matrix_1(32, 32, 8, :);
+	vector_2 = matrix_2(32, 32, 8, :);
+
+	vector_1 = vector_1(:);
+	vector_2 = vector_2(:);
+
+	figure;
+
+	plot(vector_1, 'b');
+
+	hold on;
+
+	plot(vector_2, 'r');
+
+
+end
+
+
